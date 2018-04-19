5156c60b91e656d5afeffd11a69bc8ac392f26bf
directman66 <sannikovdi@yandex.ru>
2018-04-20 00:38:02 +0500

    Add files via upload

---
+++ modules/milur/milur_devices_edit.inc.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* @version 0.1 (wizard)
+*/
+  if ($this->owner->name=='panel') {
+   $out['CONTROLPANEL']=1;
+  }
+  $table_name='milur_devices';
+  $rec=SQLSelectOne("SELECT * FROM $table_name WHERE ID='$id'");
+  if ($this->mode=='update') {
+   $ok=1;
+  //updating '<%LANG_TITLE%>' (varchar, required)
+   global $title;
+   $rec['TITLE']=$title;
+   if ($rec['TITLE']=='') {
+    $out['ERR_TITLE']=1;
+    $ok=0;
+   }
+  //updating '<%LANG_LINKED_OBJECT%>' (varchar)
+   global $linked_object;
+   $rec['LINKED_OBJECT']=$linked_object;
+  //updating '<%LANG_LINKED_PROPERTY%>' (varchar)
+   global $linked_property;
+   $rec['LINKED_PROPERTY']=$linked_property;
+  //UPDATING RECORD
+   if ($ok) {
+    if ($rec['ID']) {
+     SQLUpdate($table_name, $rec); // update
+    } else {
+     $new_rec=1;
+     $rec['ID']=SQLInsert($table_name, $rec); // adding new record
+    }
+    $out['OK']=1;
+   } else {
+    $out['ERR']=1;
+   }
+  }
+  if (is_array($rec)) {
+   foreach($rec as $k=>$v) {
+    if (!is_array($v)) {
+     $rec[$k]=htmlspecialchars($v);
+    }
+   }
+  }
+  outHash($rec, $out);
